f4e1a1f2dd6c87460ef12c863b4c851e08b29694
alexpfau <alexander.pfau@me.com>
2025-02-27 17:21:35 +0100

    Clean up repo and prep for release

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,13 @@
+import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/calendar-card-pro.js',
-  output: {
-    file: 'dist/calendar-card-pro.bundle.js',
-    format: 'iife',
-    name: 'CalendarCardPro',
-  },
-  plugins: [terser()],
+  output: [
+    {
+      file: 'dist/calendar-card-pro.js',
+      format: 'es',
+    },
+  ],
+  plugins: [resolve(), terser()],
 };
